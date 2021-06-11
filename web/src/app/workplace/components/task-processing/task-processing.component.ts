329d3f79348bfed2b017654977336d805f9db4b5
Tristan Eisermann <19949441+Tristan2357@users.noreply.github.com>
2021-06-11 11:08:57 +0200

    Populated form with variables

--- web/src/app/workplace/components/task-processing/task-processing.component.ts
+++ web/src/app/workplace/components/task-processing/task-processing.component.ts
@@ -10,6 +10,7 @@ import { ClassificationsService } from 'app/shared/services/classifications/clas
 import { take } from 'rxjs/operators';
 import { RequestInProgressService } from '../../../shared/services/request-in-progress/request-in-progress.service';
 import { GenericHttpService } from '../../../shared/services/generic-http/generic-http.service';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'taskana-task-processing',
@@ -28,6 +29,7 @@ export class TaskProcessingComponent implements OnInit, OnDestroy {
 
   @ViewChild('formParent') formParent: ElementRef;
   formHtml: SafeHtml;
+  private formFieldAttributeMap: Map<HTMLElement, string>;
 
   constructor(
     private taskService: TaskService,
@@ -38,43 +40,43 @@ export class TaskProcessingComponent implements OnInit, OnDestroy {
     private router: Router,
     private sanitizer: DomSanitizer,
     private httpService: GenericHttpService
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     this.routeSubscription = this.route.params.subscribe((params) => {
-      const {id} = params;
-      this.getTask(id);
+      const { id } = params;
+      this.getTask(id).then((task) => this.populateForm(task));
 
       this.requestInProgressService.setRequestInProgress(true);
       this.taskService
-      .claimTask(id)
-      .pipe(take(1))
-      .subscribe((task) => {
-        this.task = task;
-        this.taskService.publishUpdatedTask(task);
-        this.requestInProgressService.setRequestInProgress(false);
-      });
+        .claimTask(id)
+        .pipe(take(1))
+        .subscribe((task) => {
+          this.task = task;
+          this.taskService.publishUpdatedTask(task);
+          this.requestInProgressService.setRequestInProgress(false);
+        });
     });
   }
 
-  async getTask(id: string) {
+  async getTask(id: string): Promise<Task> {
     this.requestInProgressService.setRequestInProgress(true);
     this.task = await this.taskService.getTask(id).toPromise();
     this.taskService.selectTask(this.task);
-    const classification = await this.classificationService
-    .getClassification(this.task.classificationSummary.classificationId)
-    .toPromise();
     const formUrl = this.getFormUrl();
     if (formUrl) {
       this.loadForm(formUrl);
-    } else {
-      this.address = this.extractUrl(classification.applicationEntryPoint)
-        || `${this.address}/?q=${this.task.name}`;
+    }
+    const classification = await this.classificationService
+      .getClassification(this.task.classificationSummary.classificationId)
+      .toPromise();
+    if (!formUrl) {
+      this.address = this.extractUrl(classification.applicationEntryPoint) || `${this.address}/?q=${this.task.name}`;
       this.link = this.sanitizer.bypassSecurityTrustResourceUrl(this.address);
     }
     this.getWorkbaskets();
     this.requestInProgressService.setRequestInProgress(false);
+    return this.task;
   }
 
   getWorkbaskets() {
@@ -101,6 +103,8 @@ export class TaskProcessingComponent implements OnInit, OnDestroy {
 
   completeTask() {
     this.requestInProgressService.setRequestInProgress(true);
+    // TODO check if externalTask and then get FormData
+    // this.getFormData();
     this.taskService.completeTask(this.task.taskId).subscribe((task) => {
       this.requestInProgressService.setRequestInProgress(false);
       this.task = task;
@@ -112,21 +116,27 @@ export class TaskProcessingComponent implements OnInit, OnDestroy {
   cancelClaimTask() {
     this.requestInProgressService.setRequestInProgress(true);
     this.taskService
-    .cancelClaimTask(this.task.taskId)
-    .pipe(take(1))
-    .subscribe((task) => {
-      this.task = task;
-      this.taskService.publishUpdatedTask(task);
-      this.requestInProgressService.setRequestInProgress(false);
-    });
+      .cancelClaimTask(this.task.taskId)
+      .pipe(take(1))
+      .subscribe((task) => {
+        this.task = task;
+        this.taskService.publishUpdatedTask(task);
+        this.requestInProgressService.setRequestInProgress(false);
+      });
     this.navigateBack();
   }
 
   navigateBack() {
-    this.router.navigate([{outlets: {detail: `taskdetail/${this.task.taskId}`}}], {
+    this.router.navigate([{ outlets: { detail: `taskdetail/${this.task.taskId}` } }], {
       relativeTo: this.route.parent,
       queryParamsHandling: 'merge'
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
   }
 
   private extractUrl(url: string): string {
@@ -140,15 +150,11 @@ export class TaskProcessingComponent implements OnInit, OnDestroy {
       const parameter = expression.substring(2, expression.length - 1);
       let objectValue: any = me;
       parameter.split('.').forEach((property) => {
-        objectValue = this.getReflectiveProperty(objectValue, property);
+        objectValue = objectValue[property];
       });
       extractedUrl = extractedUrl.replace(expression, objectValue);
     });
     return extractedUrl;
-  }
-
-  private getReflectiveProperty(scope: any, property: string) {
-    return Reflect.get(scope, property);
   }
 
   private getFormUrl(): string {
@@ -161,21 +167,68 @@ export class TaskProcessingComponent implements OnInit, OnDestroy {
       if (callback.key === 'form_identifier') {
         formUrl = formUrl + callback.value;
       }
-    })
+    });
     return formUrl;
   }
 
   private loadForm(formUrl: string) {
-    this.httpService.getNonJson(formUrl).pipe(take(1)).subscribe(formHtml => {
-      console.log(formHtml);
-      this.formHtml = this.sanitizer.bypassSecurityTrustHtml(formHtml);
-      console.log(formHtml)
-    })
+    this.httpService
+      .getNonJson(formUrl)
+      .pipe(take(1))
+      .subscribe((formHtml) => {
+        this.formHtml = this.sanitizer.bypassSecurityTrustHtml(formHtml);
+        console.log(formHtml);
+      });
   }
 
-  ngOnDestroy(): void {
-    if (this.routeSubscription) {
-      this.routeSubscription.unsubscribe();
+  private populateForm(task: Task) {
+    let formFieldAttributeMap = this.getAllChildrenWithNameAttribute(this.formParent.nativeElement as HTMLElement);
+    const formVariables = this.task.callbackInfo.filter((cbInfo) => cbInfo.key === 'form_variables')[0];
+    let variablesObject = JSON.parse(formVariables.value);
+    console.log(variablesObject);
+    formFieldAttributeMap.forEach((attributeName, formElement) => {
+      const name = formElement.getAttribute(attributeName);
+      (formElement as HTMLInputElement).value = variablesObject[name];
+    });
+    this.formFieldAttributeMap = formFieldAttributeMap;
+
+    this.getFormData();
+  }
+
+  private getAllChildrenWithNameAttribute(parent: HTMLElement): Map<HTMLElement, string> {
+    const childArray = this.getFlatChildArray(parent);
+    const elementMap = new Map<HTMLElement, string>();
+    if (childArray.length) {
+      childArray.filter((child) => {
+        return !!child.getAttributeNames().filter((attributeName) => {
+          if (!!attributeName.match('name')) {
+            elementMap.set(child, attributeName);
+            return true;
+          }
+          return false;
+        });
+      });
     }
+    console.log(elementMap);
+    return elementMap;
+  }
+
+  private getFlatChildArray(parent: HTMLElement): HTMLElement[] {
+    if (parent.children?.length) {
+      let childElements = [];
+      Array.from(parent.children).map((child: HTMLElement) => {
+        childElements.push(...this.getFlatChildArray(child));
+      });
+      return childElements;
+    } else {
+      return [parent];
+    }
+  }
+
+  private getFormData() {
+    let variables = {};
+    this.formFieldAttributeMap.forEach((attributeName, formElement) => {
+      variables[formElement.getAttribute(attributeName)] = (formElement as HTMLInputElement).value;
+    });
   }
 }
